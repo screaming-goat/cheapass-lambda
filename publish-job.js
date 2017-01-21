3cc2809ad0a04e7b4a5026f53b8898908276b897
Jonas Lundberg <jonasl@catchmecode.com>
2017-01-21 14:42:15 +0100

    Adding job-publisher
    Simulating getting a text from an api,
    cleaning it up some splitting in as
    may clients that are registered.
    
    Then submitting jobs to the
    /jobs/clientId/input bucket.

---
+++ publish-job.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var firebase = require('./firebase.js');
+var database = firebase.database();
+
+const quote = `Inspirational Bible Verses - Read inspirational quotes and Scripture from the Bible that can help encourage your spirit as you in times of doubt, loss, mourning, sadness, or hopelessness. The Holy Bible has many passages that can uplift your mind and heart, giving you the strength you need to get through each day. 2 Timothy 3:16 tells us that ALL Scripture is inspired by God and useful for teaching, rebuking, correcting, and training in righteousness. The Bible is a work of God that was written by human men that were inspired by God. What better source of inspiration than our Creator! Whether you are looking for motivation, encouragement, reassurance, or peace, the Bible should be the first resource you turn to!
+	This collection of inspirational Bible verses can lead you into a deeper understanding of who God is and what His plan is for your life. He wants to "prosper you and not to harm you, to give hope and a future" (Jeremiah 29:11) You can also send these to a family member or friend in need and be assured that God can work miracles when we choose to believe His promises! Use these verses to be inspired and motivated to continue believing and having faith! For more encouragement read 30 Inspiring Christian Quotes at Crosswalk.com. These quotes will point you back to Christ and challenge you to bring Jesus into every area of your life!`;
+
+database.ref('/clients').once('value')
+	.then(clientListSnapshot => {
+		const clientList = clientListSnapshot.val();
+		console.log('Client list: ', clientList);
+		const text = quote.trim().replace(/\s{2,}/," ").replace(/[\r]?\n/," ");
+
+		const clientIds = Object.keys(clientList);
+		const textArray = text.split(/\s/);
+		const noOfWordsInChunk = Math.floor(textArray.length / clientIds.length);
+
+        const result = {};
+
+        clientIds.forEach(clientId => {
+            const words = textArray.splice(0, noOfWordsInChunk).join(" ");
+            result[clientId] = {
+                input: {
+                    words
+                }
+            };
+        });
+
+        console.log(result);
+
+        database.ref('/job/').update(result);
+	})
+	.then(() => {
+		firebase.database().goOffline();
+	});
