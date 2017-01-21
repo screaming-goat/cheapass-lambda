e52b6cf8ccbbafcd4d0b8a0df403a8ea0bfc896a
Mikael Karlsson <mmmicke@gmail.com>
2017-01-21 14:01:14 +0100

    Add word counting client code

--- code.js
+++ code.js
@@ -1,2 +1,9 @@
 
-result = baffo + 1;
+result = words.split(' ').reduce(function(map, obj) {
+    if (map[obj]) {
+        map[obj]++;
+    } else {
+        map[obj] = 1;
+    }
+    return map;
+}, {});
