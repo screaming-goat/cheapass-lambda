72cdede7eec2c88da7bb9f6e725a699fcd756ddd
Mikael Karlsson <mmmicke@gmail.com>
2017-01-21 15:13:50 +0100

    Stabilize client code snippet

--- code.js
+++ code.js
@@ -1,5 +1,11 @@
+words = words.replace(/[.|,|#]/g, ' ');
+words = words.replace(/\s\s+/g, ' ');
+words = words.toLocaleLowerCase();
 
-result = words.split(' ').reduce(function(map, obj) {
+result = words.split(' ')
+    .filter(word => word != null && word.trim() != '')
+    .reduce(function(map, obj) {
+
     if (map[obj]) {
         map[obj]++;
     } else {
